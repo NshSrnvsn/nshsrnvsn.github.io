2255321cfa365a18320eb5e50043873071b74bc9
NshSrnvsn <nsh.srnvsn@gmail.com>
2025-12-27 16:29:26 -0500

    Fix base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nshsrnvsn.github.io',
-  base: '/the-a-word',
+  base: process.env.NODE_ENV === 'production' ? '/the-a-word' : '/',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
