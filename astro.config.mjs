cda314699dc79e9152d3e8544a007090124a9602
NshSrnvsn <nsh.srnvsn@gmail.com>
2025-12-27 16:45:33 -0500

    Simplify base path to /

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nshsrnvsn.github.io',
-  base: '/the-a-word',
+  base: '/',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
