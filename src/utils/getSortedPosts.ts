c4df1bbdfe654aef1eb86ac43efe1d7ad7d65e65
NshSrnvsn <nsh.srnvsn@gmail.com>
2026-01-09 18:49:20 -0500

    Explicitly specify generic type for getSortedPosts in slug pages

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -3,9 +3,9 @@ import postFilter from "./postFilter";
 
 const getSortedPosts = <T extends "blog" | "projects">(
   posts: CollectionEntry<T>[]
-) => {
+): CollectionEntry<T>[] => {
   return posts
-    .filter(postFilter)
+    .filter(postFilter as any)
     .sort(
       (a, b) =>
         Math.floor(
@@ -14,7 +14,7 @@ const getSortedPosts = <T extends "blog" | "projects">(
         Math.floor(
           new Date(a.data.modDatetime ?? a.data.pubDatetime).getTime() / 1000
         )
-    );
+    ) as CollectionEntry<T>[];
 };
 
 export default getSortedPosts;
