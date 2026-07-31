e50c97dd572c782e793a67137d363147b92d3c33
Anusha Srinivasan <nsh.srnvsn@gmail.com>
2026-07-31 18:31:24 -0400

    merge changes

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, envField } from "astro/config";
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
@@ -18,7 +17,6 @@ export default defineConfig({
     sitemap({
       filter: page => !page.includes("/archives") || SITE.showArchives,
     }),
-    react(),
   ],
   devToolbar: {
     enabled: false,
